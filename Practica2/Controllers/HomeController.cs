c286d55fb4a1b0e763282cabce969669f50fb0fe
Estefania Pichardo <171G0242@rcarbonifera.tecnm.mx>
2020-09-15 09:32:10 -0500

    Subir proyecto

---
+++ Practica2/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Practica2.Models;
+
+namespace Practica2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Abrimos una conexion de la base de datos
+
+            mexicoContext context = new mexicoContext();
+
+            var estados = context.Estados.OrderBy(x => x.Nombre).ToList();
+            return View(estados);
+        }
+    }
+}
